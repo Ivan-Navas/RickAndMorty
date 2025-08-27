fa2491445c134f12210b1cfd8193b2be69814e67
Ivan-Navas <ivanrng.work@gmail.com>
2025-08-27 13:21:37 -0500

    feat: :sparkles: add more characters

--- src/components/Inicio.jsx
+++ src/components/Inicio.jsx
@@ -1,16 +1,37 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import { allPersonajes } from "../functions/Funtions";
+import { allPersonajes, next } from "../functions/Funtions";
 import { BsCircleFill } from "react-icons/bs";
 import loading from '../images/loading.svg'
 
 function Inicio() {
-  const [personajes, setPersonajes] = useState(null);
+  const [personajes, setPersonajes] = useState([]);
+  const [nextPage, setNextPage] = useState(2);
 
   useEffect(() => {
     allPersonajes(setPersonajes);
   }, []);
-    
+
+useEffect(() => {
+    if (nextPage >= 2) {
+      next(setPersonajes, nextPage);
+    }
+  }, [nextPage]);
+
+useEffect(() => {
+    const handleScroll = () => {
+      const documentHeight = document.documentElement.scrollHeight;
+      const scrolled = window.scrollY + window.innerHeight;
+
+      if (scrolled >= documentHeight - 5) {
+        setNextPage(prev => prev + 1);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
     <>
       {personajes != null ? (
--- src/functions/Funtions.js
+++ src/functions/Funtions.js
@@ -1,16 +1,19 @@
 import axios from 'axios'
 
 const allPersonajes = async(estado)=>{
-    const peticion = await axios.get("https://rickandmortyapi.com/api/character");
-    estado(peticion.data.results)
+  const peticion = await axios.get(`https://rickandmortyapi.com/api/character`);
+  estado(peticion.data.results)
 }
-
+const next = async(setPersonajes, nextPage) =>{
+  const peticion = await axios.get(`https://rickandmortyapi.com/api/character/?page=${nextPage}`);
+  setPersonajes((prev) => [...prev, ...peticion.data.results]);
+}
 const onePersonaje = async (id,estado)=>{
     const peticion = await axios.get(`https://rickandmortyapi.com/api/character/${id}`);
     estado(peticion.data)
 }
-
 export {
     allPersonajes,
-    onePersonaje
+    next,
+    onePersonaje,
 }
